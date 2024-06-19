2c3e8306023653cf8c6317f2264d0ff7484d7741
plutonska <wendimivo96@gmail.com>
2024-06-19 08:43:31 +0700

    update say-hello without parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/plutonska/go-say-hello
+module github.com/plutonska/go-say-hello/v2
 
 go 1.22.3
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Hello " + name + "\n"
+func SayHello() string {
+	return "Hello User"
 }
